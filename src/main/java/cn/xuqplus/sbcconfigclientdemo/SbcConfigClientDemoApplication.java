df404a5e36596f3728c6cdbc3289367a6f7cee68
xjplus <xxjplus@github.com>
2018-12-03 17:38:02 +0800

    db

--- src/main/java/cn/xuqplus/sbcconfigclientdemo/SbcConfigClientDemoApplication.java
+++ src/main/java/cn/xuqplus/sbcconfigclientdemo/SbcConfigClientDemoApplication.java
@@ -1,5 +1,8 @@
 package cn.xuqplus.sbcconfigclientdemo;
 
+import cn.xuqplus.sbcconfigclientdemo.domain.User;
+import cn.xuqplus.sbcconfigclientdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,5 +23,18 @@ public class SbcConfigClientDemoApplication {
   @GetMapping("")
   public String a() {
     return a;
+  }
+
+  @Autowired
+  UserRepository userRepository;
+
+  @GetMapping("user")
+  public String user() {
+    if (!userRepository.existsById(1L)) {
+      User user = new User();
+      user.setName("hello, 世界");
+      userRepository.save(user);
+    }
+    return userRepository.getOne(1L).toString();
   }
 }
---
+++ src/main/java/cn/xuqplus/sbcconfigclientdemo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package cn.xuqplus.sbcconfigclientdemo.repository;
+
+import cn.xuqplus.sbcconfigclientdemo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
